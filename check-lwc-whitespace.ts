39da1c32ac6738c012cc1a2376d94ca1c278c8df
David Cathcart <d.cathcart@nib.com.au>
2021-09-20 10:29:15 +1000

    Split file into lines and check each line for trailing whitespace

--- check-lwc-whitespace.ts
+++ check-lwc-whitespace.ts
@@ -1,3 +1,14 @@
 const fs = require('fs');
 const f = fs.readFileSync('C:/Dev/salesforce/unpackaged/main/default/lwc/articleSearch/articleSearch.js-meta.xml');
-console.log(f.toString());
+
+let fileContents = f.toString();
+let lines = fileContents.split('\r\n');
+
+for (let lineNo in lines) {
+    let line = lines[lineNo];
+    let trimmedLine = line.trimRight();
+    let hasTrailingWhitespace = (line != trimmedLine);
+    let trailingWhitespace = hasTrailingWhitespace ? "********* TRAILING WHITESPACE *********" : "(no trailing whitespace)";
+
+    console.log(lineNo + ': ' + lines[lineNo] + ' ' + trailingWhitespace);
+}
