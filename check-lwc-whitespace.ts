31a623142baea6b570b5db6d1354989a10d699af
David Cathcart <d.cathcart@nib.com.au>
2021-09-21 20:18:58 +1000

    Rewrite to use functions - still pretty crude, but taking tiny steps in the right direction

--- check-lwc-whitespace.ts
+++ check-lwc-whitespace.ts
@@ -1,14 +1,23 @@
 import * as fs from 'fs';
-let f = fs.readFileSync('C:/Dev/salesforce/unpackaged/main/default/lwc/articleSearch/articleSearch.js-meta.xml');
 
-let fileContents = f.toString();
-let lines = fileContents.split('\r\n');
-
-for (let lineNo in lines) {
-    let line = lines[lineNo];
-    let trimmedLine = line.trimRight();
-    let hasTrailingWhitespace = (line != trimmedLine);
-    let trailingWhitespace = hasTrailingWhitespace ? "********* TRAILING WHITESPACE *********" : "(no trailing whitespace)";
-
-    console.log(lineNo + ': ' + lines[lineNo] + ' ' + trailingWhitespace);
+function hasTrailingWhitespace(input: string) {
+    return input != input.trimRight();
 }
+
+function fileHasTrailingWhitespace(filename: string) {
+    let file = fs.readFileSync(filename);
+    let fileContents = file.toString();
+    let lines = fileContents.split('\r\n');
+
+    for (let lineNo in lines) {
+        let line = lines[lineNo];
+
+        if (hasTrailingWhitespace(line))
+            return true;
+    }
+    return false;
+}
+
+let filename = 'C:/Dev/salesforce/unpackaged/main/default/lwc/articleSearch/articleSearch.js-meta.xml';
+let result = fileHasTrailingWhitespace(filename);
+console.log(result);
