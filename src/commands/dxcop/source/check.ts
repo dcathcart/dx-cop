4d181b3173a83745afcebd55653fcbe1a0d8da7f
David Cathcart <d.cathcart@nib.com.au>
2021-10-10 16:26:48 +1100

    Copy in code from check-lwc-metadata.ts

--- src/commands/dxcop/source/check.ts
+++ src/commands/dxcop/source/check.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as os from 'os';
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
@@ -40,5 +41,37 @@ export default class Check extends SfdxCommand {
 
     // Return an object to be displayed with --json
     return { output: outputString, outputString };
+  }
+
+  public hasTrailingWhitespace(input: string) {
+      return input != input.trimRight();
+  }
+
+  public fileHasTrailingWhitespace(filename: string) {
+      let file = fs.readFileSync(filename);
+      let fileContents = file.toString();
+      let lines = fileContents.split('\r\n');
+
+      for (let lineNo in lines) {
+          let line = lines[lineNo];
+
+          if (hasTrailingWhitespace(line))
+              return true;
+      }
+      return false;
+  }
+
+  public checkLwcMetadata(folder: string) {
+      fs.readdirSync(folder).forEach(entry => {
+          if (entry == 'jsconfig.json')
+              return;
+
+          let jsMetaFileName = folder + entry + '/' + entry + '.js-meta.xml';
+
+          if (fileHasTrailingWhitespace(jsMetaFileName))
+              console.log("Trailing whitespace found: " + jsMetaFileName);
+          else
+              console.log("Passed: " + jsMetaFileName);
+      });
   }
 }
