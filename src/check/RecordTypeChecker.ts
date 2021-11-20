8f6bdb74c006f3118c67cf21a4370ddf025c496f
David Cathcart <d.cathcart@nib.com.au>
2021-11-20 18:13:58 +1100

    Added concept of 'optional' picklists - covers standard value sets, which don't always appear in record types

--- src/check/RecordTypeChecker.ts
+++ src/check/RecordTypeChecker.ts
@@ -2,7 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export class RecordTypeChecker {
-  private IGNORE_MISSING_PICKLISTS = ['PersonLeadSource', 'StageName', 'Status'];
+  private IGNORE_OBJECTS = ['Event', 'PersonAccount', 'Task'];
+  private BONUS_EXPECTED_PICKLISTS = ['Name', 'ForecastCategoryName'];
   private baseDir: string;
 
   public constructor(baseDir: string) {
@@ -10,7 +11,7 @@ export class RecordTypeChecker {
   }
 
   public run(): void {
-    const objects = this.customObjects();
+    const objects = this.customObjects().filter((o) => !this.IGNORE_OBJECTS.includes(o));
     for (const obj of objects) {
       this.checkRecordTypes(obj);
     }
@@ -20,35 +21,63 @@ export class RecordTypeChecker {
     return path.join(this.baseDir, 'objects');
   }
 
+  public objectDir(objectName: string): string {
+    return path.join(this.objectsDir(), objectName);
+  }
+
+  public fieldsDir(objectName: string): string {
+    return path.join(this.objectDir(objectName), 'fields');
+  }
+
+  public fieldFileName(objectName: string, fieldName: string): string {
+    return path.join(this.fieldsDir(objectName), fieldName + '.field-meta.xml');
+  }
+
+  public recordTypesDir(objectName: string): string {
+    return path.join(this.objectDir(objectName), 'recordTypes');
+  }
+
   public customObjects(): string[] {
     return fs.readdirSync(this.objectsDir());
   }
 
   public checkRecordTypes(objectName: string): void {
-    const objectPicklists = this.picklistFieldNames(objectName);
-    const requiredPicklists = objectPicklists.filter((i) => !this.IGNORE_MISSING_PICKLISTS.includes(i));
+    const objectPicklists = this.picklistFields(objectName);
+    const requiredPicklists = objectPicklists.filter((p) => this.hasValueSet(objectName, p));
+    const optionalPicklists = objectPicklists.filter((p) => !this.hasValueSet(objectName, p));
 
     const recordTypes = this.objectRecordTypes(objectName);
     for (const recordType of recordTypes) {
-      this.checkRecordType(objectName, recordType, requiredPicklists);
+      this.checkRecordTypePicklists(objectName, recordType, requiredPicklists, optionalPicklists);
     }
   }
 
-  public checkRecordType(objectName: string, recordTypeName: string, requiredPicklists: string[]): void {
+  public checkRecordTypePicklists(
+    objectName: string,
+    recordTypeName: string,
+    requiredPicklists: string[],
+    optionalPicklists: string[]
+  ): void {
     const recordTypePicklists = this.recordTypePicklistNames(objectName, recordTypeName);
 
-    const missingPicklists = requiredPicklists.filter((i) => !recordTypePicklists.includes(i));
+    const missingPicklists = requiredPicklists.filter((p) => !recordTypePicklists.includes(p));
     if (missingPicklists.length > 0) {
       console.log(`Record type ${objectName}.${recordTypeName} is missing picklist(s): ${missingPicklists.toString()}`);
     }
+
+    const expectedPicklists = requiredPicklists.concat(optionalPicklists).concat(this.BONUS_EXPECTED_PICKLISTS);
+    const unexpectedPicklists = recordTypePicklists.filter((p) => !expectedPicklists.includes(p));
+    if (unexpectedPicklists.length > 0) {
+      console.log(`Record type ${objectName}.${recordTypeName} contains unexpected picklist(s): ${unexpectedPicklists.toString()}`);
+    }
   }
 
-  // list of fields for an object - array of full paths to field metadata files
+  // list of fields (field names) for the given object
   public objectFields(objectName: string): string[] {
     const fieldsDir = path.join(this.objectsDir(), objectName, 'fields');
     const fields = fs.existsSync(fieldsDir) ? fs.readdirSync(fieldsDir) : [];
-
-    return fields.map((f) => path.join(fieldsDir, f));
+    const fieldNames = fields.map((f) => path.basename(f, '.field-meta.xml'));
+    return fieldNames;
   }
 
   public objectRecordTypes(objectName: string): string[] {
@@ -58,16 +87,27 @@ export class RecordTypeChecker {
     return recordTypes.map((rt) => path.basename(rt, '.recordType-meta.xml'));
   }
 
-  public picklistFieldNames(objectName: string): string[] {
+  public picklistFields(objectName: string): string[] {
     const allFields = this.objectFields(objectName);
-    const picklistFields = allFields.filter(this.isPicklistField);
+    const picklistFields = allFields.filter((f) => this.isPicklistField(objectName, f));
 
     return picklistFields.map((f) => path.basename(f, '.field-meta.xml'));
   }
 
-  public isPicklistField(fieldFileName: string): boolean {
+  public isPicklistField(objectName: string, fieldName: string): boolean {
+    const fieldFileName = this.fieldFileName(objectName, fieldName);
     const fileContents = fs.readFileSync(fieldFileName).toString();
-    return fileContents.includes('<type>Picklist</type>') || fileContents.includes('<type>MultiselectPicklist</type>');
+
+    const isPicklist = fileContents.includes('<type>Picklist</type>') || fileContents.includes('<type>MultiselectPicklist</type>');
+//    if (isPicklist) console.log(`${fileContents.length} : ${fieldFileName}`);
+    return isPicklist;
+  }
+
+  // expected: picklist value
+  public hasValueSet(objectName: string, fieldName: string): boolean {
+    const fieldFileName = this.fieldFileName(objectName, fieldName);
+    const fileContents = fs.readFileSync(fieldFileName).toString();
+    return fileContents.includes('<valueSet>');
   }
 
   public recordTypePicklistNames(objectName: string, recordTypeName: string): string[] {
