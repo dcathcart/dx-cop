ff6977cc14a3e232c9d46c53fbb00b7f31b78017
David Cathcart <d.cathcart@nib.com.au>
2022-01-26 09:56:14 +1100

    Add descriptions to MetadataError and MetadataWarning

--- src/check/MetadataProblem.ts
+++ src/check/MetadataProblem.ts
@@ -19,6 +19,8 @@ export abstract class MetadataProblem {
   }
 }
 
+// A Metadata Error is a condition that would in all likelihood prevent a component from being deployed,
+// due to a Salesforce validation error. e.g. when a record type references a picklist value that no longer exists.
 export class MetadataError extends MetadataProblem {
   public constructor(filename: string, message: string) {
     super(filename, message);
@@ -26,6 +28,12 @@ export class MetadataError extends MetadataProblem {
   }
 }
 
+// A Metadata Warning is a condition that usually *doesn't* result in a validation/deployment error, but could still
+// cause problems later. Often the result of a new component causing changes to other components, but the changes to
+// the other components have not been reflected in git. The potention 'later' problems could include:
+// - merge conflicts
+// - config intended by the developer is missed
+// - unexpected differences when retrieving component from org
 export class MetadataWarning extends MetadataProblem {
   public constructor(filename: string, message: string) {
     super(filename, message);
