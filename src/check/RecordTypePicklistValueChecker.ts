0ba02141dd56c4cc7ac3b97f44418e978c786db5
David Cathcart <d.cathcart@nib.com.au>
2022-03-18 00:17:59 +1100

    Refactor RecordTypePicklistValueChecker to use new PicklistField class. Removed a lot of code that is no longer needed as a result.

--- src/check/RecordTypePicklistValueChecker.ts
+++ src/check/RecordTypePicklistValueChecker.ts
@@ -1,28 +1,17 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { XMLParser } from 'fast-xml-parser/src/fxp';
-import { decode } from 'html-entities';
 import { RecordType } from '../metadata_browser/RecordType';
+import { PicklistField } from '../metadata_browser/PicklistField';
 import { MetadataError, MetadataProblem } from './MetadataProblem';
-
-function toArray(values: any): any[] {
-  return values instanceof Array ? values : [values];
-}
 
 export class RecordTypePicklistValueChecker {
   private IGNORE_OBJECTS = ['Event', 'PersonAccount', 'Task'];
   private IGNORE_PICKLISTS = ['ForecastCategoryName'];
 
   private baseDir: string;
-  private metadataCache: Map<string, any>;
-  private xmlParser: XMLParser;
 
   public constructor(baseDir: string) {
     this.baseDir = baseDir;
-    this.metadataCache = new Map();
-
-    // Don't trim whitespace from values. Added to handle picklist values that end with non-breaking spaces (yes, really)
-    this.xmlParser = new XMLParser({ trimValues: false });
   }
 
   // Run the record type picklist value checks. Returns an array of warning messages.
@@ -87,13 +76,14 @@ export class RecordTypePicklistValueChecker {
     picklistName: string,
     valuesInRecordType: string[]
   ): MetadataProblem[] {
-    // standard value sets not supported for now
-    if (!this.hasValueSet(objectName, picklistName)) {
+    const picklistField = new PicklistField(this.fieldFileName(objectName, picklistName));
+
+    // standard value sets / global value sets not supported for now
+    if (picklistField.usesStandardValueSet() || picklistField.usesGlobalValueSet()) {
       return [];
     }
 
-    const valuesFromObject = this.picklistValuesFromObject(picklistName, objectName);
-    const lowerCasedValuesFromObject = valuesFromObject.map((v) => v.toLowerCase());
+    const lowerCasedValuesFromObject = picklistField.activeValues().map((v) => v.toLowerCase());
 
     // Every picklist value referenced in the record type needs to be a valid value from the object's picklist definition.
     // Lower-case the values for comparison purposes, to filter out any noise from differences in casing.
@@ -115,21 +105,6 @@ export class RecordTypePicklistValueChecker {
     const fileName = this.recordTypeFileName(objectName, recordTypeName);
     const message = `Invalid value '${picklistValue}' in picklist ${picklistName}`;
     return new MetadataError(componentName, 'RecordType', fileName, message);
-  }
-
-  public picklistValuesFromObject(picklistName: string, objectName: string): string[] {
-    const fieldMetadata: any = this.parseFieldMetadata(objectName, picklistName);
-    const valueSet: any = fieldMetadata.CustomField.valueSet.valueSetDefinition;
-
-    const activeValues: any = toArray(valueSet.value).filter((v) => v.isActive !== false);
-    const values: string[] = activeValues.map((v) => this.decodeObjectPicklistValue(v.fullName.toString()));
-    return values;
-  }
-
-  public decodeObjectPicklistValue(value: string): string {
-    // HTML decode <fullName> values because that's how Salesforce encodes them in picklist field definitions.
-    // e.g. '&' is stored as '&amp;'. Note this is different from how they are encoded in record types.
-    return decode(value);
   }
 
   public objectsDir(): string {
@@ -154,33 +129,5 @@ export class RecordTypePicklistValueChecker {
 
   public recordTypeFileName(objectName: string, recordTypeName: string): string {
     return path.join(this.recordTypesDir(objectName), recordTypeName + '.recordType-meta.xml');
-  }
-
-  // TODO: Return an AnyJson
-  public parseFieldMetadata(objectName: string, fieldName: string): any {
-    return this.parseMetadataFile(this.fieldFileName(objectName, fieldName));
-  }
-
-  // TODO: Return an AnyJson
-  public parseRecordTypeMetadata(objectName: string, recordTypeName: string): any {
-    return this.parseMetadataFile(this.recordTypeFileName(objectName, recordTypeName));
-  }
-
-  public parseMetadataFile(fileName: string): any {
-    if (this.metadataCache.has(fileName)) {
-      return this.metadataCache.get(fileName);
-    } else {
-      const file: Buffer = fs.readFileSync(fileName);
-      const parsedMetadata = this.xmlParser.parse(file);
-      this.metadataCache.set(fileName, parsedMetadata);
-      return parsedMetadata;
-    }
-  }
-
-  // TODO: rewrite to used parsed XML
-  public hasValueSet(objectName: string, fieldName: string): boolean {
-    const fieldFileName = this.fieldFileName(objectName, fieldName);
-    const fileContents = fs.readFileSync(fieldFileName).toString();
-    return fileContents.includes('<valueSet>') && !fileContents.includes('<valueSetName>');
   }
 }
