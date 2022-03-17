4f1a00b1c98b8088daffb6b632ed126216429f4a
David Cathcart <d.cathcart@nib.com.au>
2022-03-17 23:06:10 +1100

    Add PicklistField.values() method

--- src/metadata_browser/PicklistField.ts
+++ src/metadata_browser/PicklistField.ts
@@ -1,4 +1,13 @@
-import { getJsonMap, hasJsonMap, hasString, JsonMap } from '@salesforce/ts-types';
+import {
+  getJsonArray,
+  getJsonMap,
+  getString,
+  hasJsonArray,
+  hasJsonMap,
+  hasString,
+  JsonArray,
+  JsonMap,
+} from '@salesforce/ts-types';
 import { MetadataComponent } from './MetadataComponent';
 
 // Class that represents a CustomField of Picklsit type
@@ -18,6 +27,44 @@ export class PicklistField extends MetadataComponent {
       return hasString(valueSet, 'valueSetName');
     } else {
       return false;
+    }
+  }
+
+  // Extract a list of picklist values from the field's XML. API Names only; we don't care about the labels.
+  // Convert this:
+  // ...
+  // <CustomField>
+  //   <fullName>Account_Status__c</fullName>
+  //   ...
+  //   <valueSet>
+  //     <valueSetDefinition>
+  //       <sorted>false</sorted>
+  //       <value>
+  //         <fullName>active</fullName>  <-- this
+  //         <default>false</default>
+  //         <label>Active</label>
+  //       </value>
+  //       <value>
+  //         <fullName>suspended</fullName>  <-- and this
+  //         ...
+  // to an array of strings: [ 'active', 'suspended', ... ]
+  public values(): string[] {
+    const customFieldElement: JsonMap = getJsonMap(this.metadata(), 'CustomField');
+    const valueSet: JsonMap = getJsonMap(customFieldElement, 'valueSet');
+    const valueSetDefinition: JsonMap = getJsonMap(valueSet, 'valueSetDefinition');
+
+    if (hasJsonArray(valueSetDefinition, 'value')) {
+      // multiple <value> elements
+      const valueArray: JsonArray = getJsonArray(valueSetDefinition, 'value');
+      return valueArray.map((v) => getString(v, 'fullName'));
+    } else if (hasJsonMap(valueSetDefinition, 'value')) {
+      // single <value> element
+      const valueMap: JsonMap = getJsonMap(valueSetDefinition, 'value');
+      const value: string = getString(valueMap, 'fullName');
+      return [value];
+    } else {
+      // no <value> elements
+      return [];
     }
   }
 }
