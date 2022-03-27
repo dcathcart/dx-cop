c2b287c69ce0791255bbe50b13de4be83968f131
David Cathcart <d.cathcart@nib.com.au>
2022-03-28 10:47:33 +1100

    Rough implementation of entitlement process version number checking

--- src/check/EntitlementProcessChecker.ts
+++ src/check/EntitlementProcessChecker.ts
@@ -4,10 +4,14 @@ import { MetadataProblem, MetadataWarning } from './MetadataProblem';
 
 export class EntitlementProcessChecker extends CheckerBase {
   public run(): MetadataProblem[] {
+    return this.checkNames().concat(this.checkVersions());
+  }
+
+  private checkNames(): MetadataProblem[] {
     return this.sfdxProjectBrowser
       .entitlementProcessVersions()
       .filter((v) => !this.hasValidName(v))
-      .map((v) => this.metadataWarning(v));
+      .map((v) => this.nameWarning(v));
   }
 
   private hasValidName(v: EntitlementProcessVersion): boolean {
@@ -22,8 +26,60 @@ export class EntitlementProcessChecker extends CheckerBase {
     return v.versionName.toLowerCase() + '_v' + v.versionNumber.toString();
   }
 
-  private metadataWarning(v: EntitlementProcessVersion): MetadataWarning {
+  private nameWarning(v: EntitlementProcessVersion): MetadataWarning {
     const message = `File name does not match metadata. Expected: '${this.expectedName(v)}'`;
     return new MetadataWarning(v.name, 'EntitlementProcess', v.fileName, message);
+  }
+
+  private checkVersions(): MetadataProblem[] {
+    const results: MetadataProblem[] = [];
+
+    const versions = this.sfdxProjectBrowser.entitlementProcessVersions();
+    const grouped = this.groupByVersionMaster(versions);
+
+    for (const group of grouped.values()) {
+      const sorted = this.sortByVersionNumber(group);
+
+      // expect version numbers to start at 1
+      const firstVersion = sorted[0];
+      if (firstVersion.versionNumber !== 1) {
+        const message = `Version numbers should start from 1 (actual: ${firstVersion.versionNumber})`;
+        results.push(new MetadataWarning(firstVersion.name, 'EntitlementProcess', firstVersion.fileName, message));
+      }
+
+      // expect version numbers to be consecutive
+      for (let i = 1; i < sorted.length; i++) {
+        const previousVersion = sorted[i - 1];
+        const thisVersion = sorted[i];
+        const expectedVersionNumber = previousVersion.versionNumber + 1;
+        const actualVersionNumber = thisVersion.versionNumber;
+
+        if (expectedVersionNumber !== actualVersionNumber) {
+          const message = `Version numbers are not consecutive. Expected ${expectedVersionNumber} but saw ${actualVersionNumber}`;
+          results.push(new MetadataWarning(thisVersion.name, 'EntitlementProcess', thisVersion.fileName, message));
+        }
+      }
+    }
+
+    return results;
+  }
+
+  // low-tech grouping of EntitlementProcessVersion objects by .versionMaster
+  // will come back later to improve, but it does the job for now
+  private groupByVersionMaster(versions: EntitlementProcessVersion[]): Map<string, EntitlementProcessVersion[]> {
+    const grouped = new Map<string, EntitlementProcessVersion[]>();
+    for (const v of versions) {
+      if (grouped.has(v.versionMaster)) {
+        grouped.get(v.versionMaster).push(v);
+      } else {
+        grouped.set(v.versionMaster, [v]);
+      }
+    }
+    return grouped;
+  }
+
+  // abstracting the code I don't like down here & will come back later
+  private sortByVersionNumber(versions: EntitlementProcessVersion[]): EntitlementProcessVersion[] {
+    return versions.sort((a, b) => a.versionNumber - b.versionNumber);
   }
 }
--- src/metadata_browser/EntitlementProcessVersion.ts
+++ src/metadata_browser/EntitlementProcessVersion.ts
@@ -1,13 +1,23 @@
 import { getNumber, getString, getJsonMap, JsonMap } from '@salesforce/ts-types';
 import { ComponentBase } from './ComponentBase';
 
+// Represents a specific version of an Entitlement Process
+// https://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_entitlementprocess.htm
 export class EntitlementProcessVersion extends ComponentBase {
   protected readonly fileExtension = 'entitlementProcess';
 
+  // Identifies the sequence of versions to which this entitlement process belongs.
+  // This field's contents can be any value as long as it is identical among all versions of the entitlement process.
+  public get versionMaster(): string {
+    return getString(this.rootNode, 'versionMaster');
+  }
+
+  // The name of the entitlement process as it displays in the user interface.
   public get versionName(): string {
     return getString(this.rootNode, 'name');
   }
 
+  // The version number of the entitlement process. Must be 1 or greater.
   public get versionNumber(): number {
     return getNumber(this.rootNode, 'versionNumber');
   }
