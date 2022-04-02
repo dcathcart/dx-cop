921f2edd14f7cf44de8777c9bf67c3c4ecd67321
David Cathcart <d.cathcart@nib.com.au>
2022-04-03 00:34:43 +1100

    Check individual permissions in <objectPermissions> - make sure they're all set to true for the Admin profile

--- src/check/AdminProfileChecker.ts
+++ src/check/AdminProfileChecker.ts
@@ -1,4 +1,4 @@
-import { Profile } from '../metadata_browser/Profile';
+import { Profile, ProfileObjectPermission } from '../metadata_browser/Profile';
 import { CheckerBase } from './CheckerBase';
 import { MetadataProblem, MetadataWarning } from './MetadataProblem';
 
@@ -7,10 +7,10 @@ export class AdminProfileChecker extends CheckerBase {
     const adminProfile = this.sfdxProjectBrowser.profileByName('Admin');
     const objectNames = this.sfdxProjectBrowser.customObjects().map((obj) => obj.name);
 
-    return this.ensureObjectsExist(adminProfile, objectNames);
+    return this.missingObjects(adminProfile, objectNames).concat(this.missingObjectPermissions(adminProfile));
   }
 
-  private ensureObjectsExist(profile: Profile, objectNames: string[]): MetadataProblem[] {
+  private missingObjects(profile: Profile, objectNames: string[]): MetadataProblem[] {
     const objectNamesInProfile = profile.objectPermissions().map((p) => p.objectName);
     const missingObjectNames = objectNames.filter((n) => !objectNamesInProfile.includes(n));
     return missingObjectNames.map((n) => this.missingObjectError(profile, n));
@@ -18,6 +18,42 @@ export class AdminProfileChecker extends CheckerBase {
 
   private missingObjectError(profile: Profile, objectName: string): MetadataWarning {
     const message = `<objectPermissions> not found for ${objectName}`;
+    return new MetadataWarning(profile.name, 'Profile', profile.fileName, message);
+  }
+
+  private missingObjectPermissions(profile: Profile): MetadataProblem[] {
+    const results: MetadataProblem[] = [];
+
+    for (const objectPermission of profile.objectPermissions()) {
+      if (!objectPermission.allowCreate) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'allowCreate'));
+      }
+      if (!objectPermission.allowDelete) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'allowDelete'));
+      }
+      if (!objectPermission.allowEdit) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'allowEdit'));
+      }
+      if (!objectPermission.allowRead) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'allowRead'));
+      }
+      if (!objectPermission.modifyAllRecords) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'modifyAllRecords'));
+      }
+      if (!objectPermission.viewAllRecords) {
+        results.push(this.missingObjectPermissionError(profile, objectPermission, 'viewAllRecords'));
+      }
+    }
+
+    return results;
+  }
+
+  private missingObjectPermissionError(
+    profile: Profile,
+    objectPermission: ProfileObjectPermission,
+    permissionName: string
+  ): MetadataProblem {
+    const message = `<${permissionName}> permission not set for object ${objectPermission.objectName}`;
     return new MetadataWarning(profile.name, 'Profile', profile.fileName, message);
   }
 }
