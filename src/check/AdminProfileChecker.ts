71ba059d7d7cca6cc3afe9dddb87107b5bc73808
David Cathcart <d.cathcart@nib.com.au>
2022-04-03 13:03:43 +1000

    Limit scope further, to only check fields for custom objects - avoid standard objects, custom metadata types, event objects etc

--- src/check/AdminProfileChecker.ts
+++ src/check/AdminProfileChecker.ts
@@ -1,4 +1,5 @@
 import { CustomField } from '../metadata_browser/CustomField';
+import { CustomObject } from '../metadata_browser/CustomObject';
 import { Profile, ProfileObjectPermission } from '../metadata_browser/Profile';
 import { CheckerBase } from './CheckerBase';
 import { MetadataProblem, MetadataWarning } from './MetadataProblem';
@@ -6,37 +7,37 @@ import { MetadataProblem, MetadataWarning } from './MetadataProblem';
 export class AdminProfileChecker extends CheckerBase {
   public run(): MetadataProblem[] {
     const adminProfile = this.sfdxProjectBrowser.profileByName('Admin');
-    const objectNames = this.sfdxProjectBrowser.customObjects().map((obj) => obj.name);
+    const customObjects = this.sfdxProjectBrowser.customObjects();
 
-    const allFields: CustomField[] = [];
-    for (const objectName of this.sfdxProjectBrowser.objectNames()) {
-      allFields.push(...this.sfdxProjectBrowser.customFields(objectName));
+    const customFields: CustomField[] = [];
+    for (const obj of customObjects) {
+      customFields.push(...this.sfdxProjectBrowser.customFields(obj.name));
     }
 
-    return this.missingFields(adminProfile, allFields)
-      .concat(this.missingObjects(adminProfile, objectNames))
+    return this.missingFields(adminProfile, customFields)
+      .concat(this.missingObjects(adminProfile, customObjects))
       .concat(this.missingObjectPermissions(adminProfile));
   }
 
   private missingFields(profile: Profile, customFields: CustomField[]): MetadataProblem[] {
     const fieldNamesInProfile = profile.fieldPermissions().map((p) => p.objectFieldName);
-    const missingFieldNames = customFields.filter((n) => !fieldNamesInProfile.includes(n.objectFieldName));
-    return missingFieldNames.map((n) => this.missingFieldError(profile, n.objectFieldName));
+    const missingFields = customFields.filter((f) => !fieldNamesInProfile.includes(f.objectFieldName));
+    return missingFields.map((f) => this.missingFieldError(profile, f));
   }
 
-  private missingFieldError(profile: Profile, objectDotfieldName: string): MetadataWarning {
-    const message = `<fieldPermissions> not found for ${objectDotfieldName}`;
+  private missingFieldError(profile: Profile, customField: CustomField): MetadataWarning {
+    const message = `<fieldPermissions> not found for ${customField.objectFieldName}`;
     return new MetadataWarning(profile.name, 'Profile', profile.fileName, message);
   }
 
-  private missingObjects(profile: Profile, objectNames: string[]): MetadataProblem[] {
+  private missingObjects(profile: Profile, customObjects: CustomObject[]): MetadataProblem[] {
     const objectNamesInProfile = profile.objectPermissions().map((p) => p.objectName);
-    const missingObjectNames = objectNames.filter((n) => !objectNamesInProfile.includes(n));
-    return missingObjectNames.map((n) => this.missingObjectError(profile, n));
+    const missingObjects = customObjects.filter((obj) => !objectNamesInProfile.includes(obj.name));
+    return missingObjects.map((obj) => this.missingObjectError(profile, obj));
   }
 
-  private missingObjectError(profile: Profile, objectName: string): MetadataWarning {
-    const message = `<objectPermissions> not found for ${objectName}`;
+  private missingObjectError(profile: Profile, customObject: CustomObject): MetadataWarning {
+    const message = `<objectPermissions> not found for ${customObject.name}`;
     return new MetadataWarning(profile.name, 'Profile', profile.fileName, message);
   }
 
