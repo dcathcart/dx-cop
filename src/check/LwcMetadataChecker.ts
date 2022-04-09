800caa5e74366199662cceb150aa9327f4e788ca
David Cathcart <d.cathcart@nib.com.au>
2022-04-09 16:37:40 +1000

    Refactor LwcMetadataChecker & add unit tests

--- src/check/LwcMetadataChecker.ts
+++ src/check/LwcMetadataChecker.ts
@@ -4,55 +4,38 @@ import { CheckerBase } from './CheckerBase';
 import { MetadataProblem, MetadataWarning } from './MetadataProblem';
 
 export class LwcMetadataChecker extends CheckerBase {
+  // Checks over all lightning web components in the project.
+  // Right now the only check performed is for trailing whitespace.
+  // Any trailing whitespace in a .js-meta.xml file can, for reasons unknown, cause even more whitespace to be added
+  // *between* lines during a Salesforce deployment (which can result in noisy diffs later when the lwc is retrieved).
+  // Best to avoid in the first place.
   public run(): MetadataProblem[] {
     const lwcBundles = this.sfdxProjectBrowser.lwcBundles();
-    return this.checkLwcFolders(lwcBundles);
+    return this.trailingWhitespaceWarnings(lwcBundles);
   }
 
-  // Checks over all of the given lightning web components.
-  // Right now the only check performed is for trailing whitespace. Any trailing whitespace in a .js-meta.xml file
-  // can, for reasons unknown, cause even more whitespace to be added _between_ lines during a Salesforce deployment
-  // (which can result in noisy diffs later when the lwc is retrieved). Best to avoid in the first place.
-  private checkLwcFolders(lwcBundles: LightningComponentBundle[]): MetadataProblem[] {
-    const warnings: MetadataProblem[] = [];
-
-    lwcBundles.forEach((lwc) => {
-      warnings.push(...this.checkJsMetaFile(lwc.name, lwc.jsMetaFileName()));
-    });
-
-    return warnings;
-  }
-
-  // Check a single .js-meta.xml file (full path expected).
-  // Returns an array of warnings about the file.
-  // (okay, so right now there will only every be zero or one warnings, because we only check for one thing,
-  // i.e. trailing whitespace. Writing it this way with the future in mind)
-  private checkJsMetaFile(lwcName: string, jsMetaFilename: string): MetadataProblem[] {
-    const warnings: MetadataProblem[] = [];
-
-    if (this.fileHasTrailingWhitespace(jsMetaFilename)) {
-      warnings.push(
-        new MetadataWarning(
-          lwcName,
-          'LightningComponentBundle',
-          jsMetaFilename,
-          'Whitespace characters detected at the end of one or more lines in .js-meta.xml file'
-        )
-      );
-    }
-
-    return warnings;
+  private trailingWhitespaceWarnings(lwcBundles: LightningComponentBundle[]): MetadataProblem[] {
+    return lwcBundles
+      .filter((lwc) => this.fileHasTrailingWhitespace(lwc.jsMetaFileName()))
+      .map((lwc) => this.trailingWhitespaceWarning(lwc));
   }
 
   private fileHasTrailingWhitespace(filename: string): boolean {
-    const file = fs.readFileSync(filename);
-    const fileContents = file.toString();
-    const lines = fileContents.split(/\r\n|\n|\r/); // handle DOS|Unix|old Mac line endings
-    const result = lines.some((line) => this.hasTrailingWhitespace(line));
-    return result;
+    const file: Buffer = fs.readFileSync(filename);
+    const lines: string[] = file.toString().split(/\r\n|\n|\r/); // handle DOS|Unix|old Mac line endings
+    return lines.some((line) => this.lineHasTrailingWhitespace(line));
   }
 
-  private hasTrailingWhitespace(input: string): boolean {
-    return input !== input.trimEnd();
+  private lineHasTrailingWhitespace(line: string): boolean {
+    return line !== line.trimEnd();
+  }
+
+  private trailingWhitespaceWarning(lwc: LightningComponentBundle): MetadataWarning {
+    return new MetadataWarning(
+      lwc.name,
+      lwc.metadataType,
+      lwc.jsMetaFileName(),
+      'Whitespace characters detected at the end of one or more lines in .js-meta.xml'
+    );
   }
 }
--- src/metadata_browser/LightningComponentBundle.ts
+++ src/metadata_browser/LightningComponentBundle.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 // Abstract base class for all metadata components
 export class LightningComponentBundle {
   // The Salesforce metadata type, according to https://developer.salesforce.com/docs/atlas.en-us.234.0.api_meta.meta/api_meta/meta_types_list.htm
-  protected readonly metadataType = 'LightningComponentBundle';
+  public readonly metadataType = 'LightningComponentBundle';
 
   private readonly lwcFolder: string;
 
