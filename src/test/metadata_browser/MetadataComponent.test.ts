7509c4a0aa0c2dd5a86cd022a2e810ff7f1cc04c
David Cathcart <d.cathcart@nib.com.au>
2022-04-15 14:09:37 +1000

    Move Text__c field that also got missed in the rebase

--- src/test/metadata_browser/MetadataComponent.test.ts
+++ src/test/metadata_browser/MetadataComponent.test.ts
@@ -5,8 +5,9 @@ import { CustomField } from '../../metadata_browser/CustomField';
 describe('ObjectSubComponent', () => {
   describe('.objectName()', () => {
     it('should return the name of the object, derived from the file path', () => {
-      const field = new CustomField('src/test/fixtures/objects/TestObject/fields/Text__c.field-meta.xml');
-      expect(field.objectName).to.equal('TestObject');
+      // note: file doesn't need to exist for this test to pass
+      const field = new CustomField('src/test/fixtures/objects/TestObject1/fields/Text__c.field-meta.xml');
+      expect(field.objectName).to.equal('TestObject1');
     });
   });
 });
