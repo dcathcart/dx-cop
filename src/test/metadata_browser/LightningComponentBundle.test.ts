64fb451f1370ae0c9bc7c0b0aa5e86a1d87c3ab4
David Cathcart <d.cathcart@nib.com.au>
2022-04-16 15:05:28 +1000

    Add test for LightningComponentBundle.baseFolder property

--- src/test/metadata_browser/LightningComponentBundle.test.ts
+++ src/test/metadata_browser/LightningComponentBundle.test.ts
@@ -4,6 +4,18 @@ import { expect } from 'chai';
 import { LightningComponentBundle } from '../../metadata_browser/LightningComponentBundle';
 
 describe('LightningComponentBundle', () => {
+  describe('.baseFolder property', () => {
+    it('should contain the base folder for the bundle, with correct (platform-specific) path separators', () => {
+      const unixPath = 'src/test/fixtures/lwc/GoodExample';
+      const lwc1 = new LightningComponentBundle(unixPath);
+      expect(lwc1.baseFolder).to.equal(path.normalize(unixPath));
+
+      const windowsPath = 'src\\test\\fixtures\\lwc\\GoodExample';
+      const lwc2 = new LightningComponentBundle(windowsPath);
+      expect(lwc2.baseFolder).to.equal(path.normalize(windowsPath));
+    });
+  });
+
   describe('.name property', () => {
     it('should return the name of the bundle, derived from the path', () => {
       const lwc = new LightningComponentBundle('src/test/fixtures/lwc/GoodExample');
