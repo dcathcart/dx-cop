dc076428560f71c37cffa356aadf35f841607c79
David Cathcart <d.cathcart@nib.com.au>
2022-04-18 23:25:25 +1000

    Finish tests for Profile.ts, mocking metadata where needed

--- src/test/metadata_browser/Profile.test.ts
+++ src/test/metadata_browser/Profile.test.ts
@@ -1,22 +1,71 @@
 import 'mocha';
 import { expect } from 'chai';
-import { ProfileFieldPermission, ProfileObjectPermission } from '../../metadata_browser/Profile';
+import * as Sinon from 'sinon';
+import { JsonMap } from '@salesforce/ts-types';
+import { Profile, ProfileFieldPermission, ProfileObjectPermission } from '../../metadata_browser/Profile';
+
+describe('Profile', () => {
+  describe('.fieldPermissions()', () => {
+    it('should return an array of ProfileFieldPermission objects', () => {
+      const json: JsonMap = {
+        fieldPermissions: [
+          { editable: false, field: 'TestObject.Field1__c', readable: true },
+          { editable: false, field: 'TestObject.Field2__c', readable: true },
+        ],
+      };
+      const profile = new Profile('');
+      Sinon.stub(profile, 'metadata').get(() => json);
+
+      const results = profile.fieldPermissions();
+      expect(results.length).to.equal(2);
+      expect(results[0].objectFieldName).to.equal('TestObject.Field1__c');
+      expect(results[1].objectFieldName).to.equal('TestObject.Field2__c');
+    });
+  });
+
+  describe('.objectPermissions()', () => {
+    it('should return an array of ProfileObjectPermission objects', () => {
+      const json: JsonMap = {
+        objectPermissions: [
+          { allowEdit: false, allowRead: true, object: 'TestObject1__c' },
+          { allowEdit: false, allowRead: true, object: 'TestObject2__c' },
+        ],
+      };
+      const profile = new Profile('');
+      Sinon.stub(profile, 'metadata').get(() => json);
+
+      const results = profile.objectPermissions();
+      expect(results.length).to.equal(2);
+      expect(results[0].objectName).to.equal('TestObject1__c');
+      expect(results[1].objectName).to.equal('TestObject2__c');
+    });
+  });
+});
 
 describe('ProfileFieldPermission', () => {
   it('should return correct values for all properties', () => {
-    const json = { editable: false, field: 'TestObject.TestField__c', readable: true };
-    const result = new ProfileFieldPermission(json);
-    expect(result.editable).to.equal(false, 'editable');
-    expect(result.readable).to.equal(true, 'readable');
-    expect(result.objectFieldName).to.equal('TestObject.TestField__c', 'objectFieldName');
-    expect(result.objectName()).to.equal('TestObject', 'objectName()');
-    expect(result.fieldName()).to.equal('TestField__c', 'fieldName()');
+    const fieldPerm = new ProfileFieldPermission({
+      editable: false,
+      field: 'TestObject.TestField__c',
+      readable: true,
+    });
+    expect(fieldPerm.objectFieldName).to.equal('TestObject.TestField__c', 'objectFieldName');
+    expect(fieldPerm.editable).to.equal(false, 'editable');
+    expect(fieldPerm.readable).to.equal(true, 'readable');
+  });
+  it('should derive the object name from the composite object/field property', () => {
+    const fieldPerm = new ProfileFieldPermission({ field: 'ObjectName.FieldName' });
+    expect(fieldPerm.objectName()).to.equal('ObjectName');
+  });
+  it('should derive the field name from the composite object/field property', () => {
+    const fieldPerm = new ProfileFieldPermission({ field: 'ObjectName.FieldName' });
+    expect(fieldPerm.fieldName()).to.equal('FieldName');
   });
 });
 
 describe('ProfileObjectPermission', () => {
   it('should return correct values for all properties', () => {
-    const json = {
+    const objPerm = new ProfileObjectPermission({
       allowCreate: false,
       allowDelete: false,
       allowEdit: true,
@@ -24,14 +73,13 @@ describe('ProfileObjectPermission', () => {
       modifyAllRecords: false,
       object: 'TestObject',
       viewAllRecords: true,
-    };
-    const result = new ProfileObjectPermission(json);
-    expect(result.allowCreate).to.equal(false, 'allowCreate');
-    expect(result.allowDelete).to.equal(false, 'allowDelete');
-    expect(result.allowEdit).to.equal(true, 'allowEdit');
-    expect(result.allowRead).to.equal(true, 'allowRead');
-    expect(result.modifyAllRecords).to.equal(false, 'modifyAllRecords');
-    expect(result.objectName).to.equal('TestObject', 'objectName');
-    expect(result.viewAllRecords).to.equal(true, 'viewAllRecords');
+    });
+    expect(objPerm.allowCreate).to.equal(false, 'allowCreate');
+    expect(objPerm.allowDelete).to.equal(false, 'allowDelete');
+    expect(objPerm.allowEdit).to.equal(true, 'allowEdit');
+    expect(objPerm.allowRead).to.equal(true, 'allowRead');
+    expect(objPerm.modifyAllRecords).to.equal(false, 'modifyAllRecords');
+    expect(objPerm.objectName).to.equal('TestObject', 'objectName');
+    expect(objPerm.viewAllRecords).to.equal(true, 'viewAllRecords');
   });
 });
