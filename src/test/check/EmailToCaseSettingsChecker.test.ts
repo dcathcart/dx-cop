2fb57b9054f896706001ee73fb5b514bcf7c5b52
David Cathcart <d.cathcart@nib.com.au>
2022-04-20 00:02:48 +1000

    Update other tests for EmailToCaseSettingsChecker.sortOrderWarnings(). Stub various inputs like the new test that was just added

--- src/test/check/EmailToCaseSettingsChecker.test.ts
+++ src/test/check/EmailToCaseSettingsChecker.test.ts
@@ -80,16 +80,23 @@ describe('EmailToCaseSettingsChecker', () => {
 
   describe('.sortOrderWarnings()', () => {
     it("should return a metadata error when <routingAddresses> aren't sorted by <routingName>", () => {
-      const fileName = path.normalize('src/test/fixtures/settings/Case-errors.settings-meta.xml');
-      const emailToCaseSettings = new EmailToCaseSettings(fileName);
+      const routingAddresses = [
+        new EmailToCaseRoutingAddress({ routingName: 'Entry 1' }),
+        new EmailToCaseRoutingAddress({ routingName: 'Entry 3' }),
+        new EmailToCaseRoutingAddress({ routingName: 'Entry 2' }),
+      ];
+      const emailToCaseSettings = new EmailToCaseSettings('');
+      sinon.stub(emailToCaseSettings, 'routingAddresses').returns(routingAddresses);
+      sinon.stub(emailToCaseSettings, 'fileName').get(() => 'Case.settings-meta.xml');
+
       const checker = new EmailToCaseSettingsChecker(null);
       const result = checker['sortOrderWarnings'](emailToCaseSettings);
       expect(result.length).to.equal(1);
       expect(result[0].componentType).to.equal('CaseSettings');
       expect(result[0].componentName).to.equal('EmailToCase');
-      expect(result[0].fileName).to.equal(fileName);
+      expect(result[0].fileName).to.equal('Case.settings-meta.xml');
       expect(result[0].problem).to.equal(
-        "<routingAddresses> should be sorted by <routingName>. Expect 'Test email address 2' to be before 'Test email address 3'"
+        "<routingAddresses> should be sorted by <routingName>. Expect 'Entry 2' to be before 'Entry 3'"
       );
       expect(result[0].problemType).to.equal('Warning');
     });
