681c3a75d62fd205598174be23a5ad672fa4bc62
David Cathcart <d.cathcart@nib.com.au>
2022-04-24 22:08:28 +1000

    Add more tests & improve existing ones

--- src/test/commands/check.test.ts
+++ src/test/commands/check.test.ts
@@ -1,25 +1,42 @@
 import * as fs from 'fs';
 import 'mocha';
 import { expect } from 'chai';
-// import * as sinon from 'sinon';
 import Check from '../../commands/dxcop/source/check';
 
 describe('dxcop:source:check command', () => {
   describe('.loadConfig()', () => {
-    it('should load config from .dxcoprc file if it exists', () => {
-      fs.copyFileSync('src/test/commands/sample.dxcoprc', '.dxcoprc');
-      const checkCommand = new Check([], null);
-      const config = checkCommand['loadConfig']();
-      expect(config.ruleSets.emailToCaseSettings.enabled).to.equal(false);
-      expect(config.ruleSets.recordTypePicklistValues.enabled).to.equal(true);
-    });
+    context('when a .dxcoprc config file exists', () => {
+      before(() => {
+        fs.copyFileSync('src/test/commands/sample.dxcoprc', '.dxcoprc');
+      });
 
-    it('should load default config if no config file exists', () => {
-      fs.rmSync('.dxcoprc');
+      it('should load config from the file', () => {
+        fs.copyFileSync('src/test/commands/sample.dxcoprc', '.dxcoprc');
+        const checkCommand = new Check([], null);
+        const config = checkCommand['loadConfig']();
+        expect(config.ruleSets.emailToCaseSettings.enabled).to.equal(false);
+        expect(config.ruleSets.recordTypePicklistValues.enabled).to.equal(true);
+      });
+
+      after(() => {
+        fs.rmSync('.dxcoprc');
+      });
+    });
+    context('when there is no .dxcoprc config file', () => {
+      it('should load the default config', () => {
+        const checkCommand = new Check([], null);
+        const config = checkCommand['loadConfig']();
+        expect(config.ruleSets.emailToCaseSettings.enabled).to.equal(true);
+        expect(config.ruleSets.recordTypePicklistValues.enabled).to.equal(true);
+      });
+    });
+  });
+
+  describe('.ruleSets()', () => {
+    it('should create a ruleset object for each ruleset enabled in the (default) config', () => {
       const checkCommand = new Check([], null);
-      const config = checkCommand['loadConfig']();
-      expect(config.ruleSets.emailToCaseSettings.enabled).to.equal(true);
-      expect(config.ruleSets.recordTypePicklistValues.enabled).to.equal(true);
+      const ruleSets = checkCommand['ruleSets'](null);
+      expect(ruleSets.length).to.equal(4);
     });
   });
 });
