a203f14ae2586206cfde0ba448ba8def2eef731b
David Cathcart <d.cathcart@nib.com.au>
2023-01-01 22:23:32 +1100

    Rearrange QueueRuleset tests - move queue metadata to separate files

--- src/ruleset/queueRuleset.test.ts
+++ src/ruleset/queueRuleset.test.ts
@@ -7,40 +7,18 @@ import { QueueRuleset } from './queueRuleset';
 
 describe('QueueRuleset', () => {
   describe('.run()', () => {
-    const queueWithNoUsersXml = `<?xml version="1.0" encoding="UTF-8"?>
-    <Queue xmlns="http://soap.sforce.com/2006/04/metadata">
-        <doesSendEmailToMembers>false</doesSendEmailToMembers>
-        <name>Test</name>
-        <queueSobject>
-            <sobjectType>Case</sobjectType>
-        </queueSobject>
-    </Queue>`;
-
-    const queueWithUsersXml = `<?xml version="1.0" encoding="UTF-8"?>
-    <Queue xmlns="http://soap.sforce.com/2006/04/metadata">
-        <doesSendEmailToMembers>false</doesSendEmailToMembers>
-        <name>Test</name>
-        <queueMembers>
-            <users>
-                <user>test@test.com</user>
-            </users>
-        </queueMembers>
-        <queueSobject>
-            <sobjectType>Case</sobjectType>
-        </queueSobject>
-    </Queue>`;
-
-    const queueWithNoUsers = new Queue('Test1.queue-meta.xml', queueWithNoUsersXml);
-    const queueWithUsers = new Queue('Test2.queue-meta.xml', queueWithUsersXml);
+    const queueWithNoMembers = new Queue('src/test/metadata/queues/Queue_With_No_Members.queue-meta.xml');
+    const queueWithUsers = new Queue('src/test/metadata/queues/Queue_With_Users.queue-meta.xml');
 
     it('returns a warning if a queue has users', () => {
       const sfdxProjectBrowser = new SfdxProjectBrowser(null);
-      sinon.stub(sfdxProjectBrowser, 'queues').returns([queueWithNoUsers, queueWithUsers]);
+      sinon.stub(sfdxProjectBrowser, 'queues').returns([queueWithNoMembers, queueWithUsers]);
 
       const ruleset = new QueueRuleset(sfdxProjectBrowser);
       const results = ruleset.run();
 
       expect(results.length).to.equal(1);
+      expect(results[0].problem).to.equal('Users should not be direct members of queues');
     });
   });
 });
